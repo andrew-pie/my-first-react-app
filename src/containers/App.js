903ca24ebdeaee2a3efb8c1c099e7ffa7b392134
Andrii Pyrozhok <pyrozhok.dev@gmail.com>
2024-02-13 08:57:41 -0700

    added fixed search, organized files on folders, build app

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import Cardlist from "./Cardlist";
-import SearchBox from './SearchBox';
+import Cardlist from "../components/Cardlist";
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
 import './App.css';
 
 class App extends Component {
@@ -24,21 +25,23 @@ class App extends Component {
 	}
 
 	render() {
-		const filteredRobots = this.state.robots.filter(robots => {
-			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+		const { robots, searchfield } = this.state;
+		const filteredRobots = robots.filter(robot => {
+			return robot.name.toLowerCase().includes(searchfield.toLowerCase());
 		})
-		if (this.state.robots.length === 0) {
-			return <h1>Loading</h1>
-		} else {
-			return (
+		return !robots.length ?
+			<h1>Loading</h1> :
+			(
 				<div className="tc">
 					<h1 className="f2">RoboFriends</h1>
 					<SearchBox searchChange={this.onSearchChange} />
-					<Cardlist robots={filteredRobots} />
+					<Scroll>
+						<Cardlist robots={filteredRobots} />
+					</Scroll>
 				</div>
 			);
-		}
 	}
 }
+
 
 export default App;
